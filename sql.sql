ef780e00b2e8e2628fa4580cdf28491cc72eb3b1
RubenPallin <113434110+RubenPallin@users.noreply.github.com>
2022-11-22 13:28:42 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -19,11 +19,11 @@ insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ("Mar
 insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ("Percebesa", "Mayor galardonada con el bogabante de oro.", "La talaya del Arcipreste");
 insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ("Corme Calidade", "Bien situado, en Corme solo hay un puerto y una iglesia", "Corme cidade");
 insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ("Enzzo's", "Promoción de ONCE pizzas.", "Howkings");
-insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ()
+insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ("Marisquería Cavaret", "Buena marisquería", "Calle Ave de Paraíso Nº7");
+insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ("Max & Henry", "Los leones de Montepinar", "Plaza Mayor");
+insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ("El bar l", "Buena marisquería", "Calle Segovia");
+insert into Restaurante(nombre_restaurante, descripcion, direccion) VALUES ("Conchería Paki", "Paki le sabe", "Callao");
 
 insert into Usuario(nombre_usuario, email, contraseña) VALUES ("Billy Daniel", "bdanielh@fpcoruna.afundacion.org", "pepellin");
 insert into Usuario(nombre_usuario, email, contraseña) VALUES ("Pablo Motos", "elhormiguero@gmail.com", "trancasybarrancas");
-insert into Usuario(nombre_usuario, email, contraseña) VALUES ("Alberto Chicote", "pesadillaenlacocina@gmail.com", "cristinapedroche")
-
-
-
+insert into Usuario(nombre_usuario, email, contraseña) VALUES ("Alberto Chicote", "pesadillaenlacocina@gmail.com", "cristinapedroche");
